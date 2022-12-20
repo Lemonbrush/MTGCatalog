1243befb372d135f4098cfdfb68e07278d0a911e
Alexander <aa_rubtsov@mail.ru>
2022-12-21 00:19:20 +0300

    Initial Commit

---
+++ MagicCatalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagicCatalog
+//
+//  Created by Alexander Rubtsov on 21.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MagicCatalog/MagicCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicCatalogApp.swift
+//  MagicCatalog
+//
+//  Created by Alexander Rubtsov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MagicCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
